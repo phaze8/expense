0aa0ecd5e320b0a498c2ff68a566dbf2e0cd1d39
Julia Feys <julia.feys@valorem-energy.com>
2017-05-12 15:42:58 -0500

    removed person

--- db/migrate/20170508221627_add_temp_to_person.rb
+++
@@ -1,5 +0,0 @@
-class AddTempToPerson < ActiveRecord::Migration[5.1]
-  def change
-    add_column :people, :temp, :string
-  end
-end
--- db/migrate/20170509131525_add_number_of_people_to_costs.rb
+++
@@ -1,6 +0,0 @@
-class AddNumberOfPeopleToCosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :costs, :number_of_people, :integer
-    add_column :costs, :people_names, :string
-  end
-end
