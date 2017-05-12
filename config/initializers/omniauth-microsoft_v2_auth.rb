c738e721ef3382a595390c61dcbc466d35b5fa01
Julia Feys <julia.feys@valorem-energy.com>
2017-05-12 15:08:49 -0500

    added if Rails.env.development?

--- config/initializers/omniauth-microsoft_v2_auth.rb
+++ config/initializers/omniauth-microsoft_v2_auth.rb
@@ -1,5 +1,5 @@
 require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :microsoft_v2_auth,
